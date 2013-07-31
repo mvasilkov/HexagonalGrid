54d41ec6eeadbc4c190baf8de172a747a3341c9e
Mark Vasilkov <mvasilkov@gmail.com>
2013-07-31 22:48:28 +0300

    unordered_set

--- cpp_basic/program.cpp
+++ cpp_basic/program.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <string>
+#include <unordered_set>
 
 extern "C" {
     extern void canvas_getContext();
@@ -13,12 +15,27 @@ static const float size_u {sqrtf(3) * size};
 static const float size_v {2 * size};
 static const float pad {10.5};
 
+static std::unordered_set<std::u16string> filter;
+static char16_t xx_u, xx_v;
+
 static inline void moveto(float u, float v) {
     canvas_moveTo(u, v);
+    xx_u = static_cast<char16_t>(u);
+    xx_v = static_cast<char16_t>(v);
 }
 
 static inline void lineto(float u, float v) {
+    char16_t chr_u {static_cast<char16_t>(u)};
+    char16_t chr_v {static_cast<char16_t>(v)};
+    std::u16string k {xx_u, xx_v, chr_u, chr_v};
+
+    if (filter.count(k)) { moveto(u, v); return; }
+
+    k.assign({chr_u, chr_v, xx_u, xx_v});
+    filter.insert(std::move(k));
     canvas_lineTo(u, v);
+    xx_u = chr_u;
+    xx_v = chr_v;
 }
 
 static inline void paint_cell(float u, float v) {
